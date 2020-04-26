c509f26baffc4a74fa4d4a904562cb8870ba6fe1
Jimmy Ben Klieve <j.b.klieve@live.cn>
2020-04-26 13:09:59 +0800

    feat: add dark theme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import MainLayout from 'layouts/MainLayout';
 
 function App(props) {
+  React.useEffect(() => {
+    const darkListener = (mq: MediaQueryListEvent) => {
+      if (mq.matches) {
+        document.documentElement.classList.add('theme-dark');
+      }
+      else {
+        document.documentElement.classList.remove('theme-dark');
+      }
+    };
+
+    const media = window.matchMedia('(prefers-color-scheme: dark)');
+
+    media.addEventListener('change', darkListener);
+    darkListener(media);
+
+    return () => {
+      media.removeEventListener('change', darkListener);
+    }
+  }, []);
+
   return (
     <Router>
       <Switch>
