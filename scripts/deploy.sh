91054c95af6b61e451ec807dace92fbf85f59ce8
UN1C0DE <j.b.klieve@live.cn>
2020-06-22 15:05:04 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
 mv ./build ./un1c0de.xyz
+tar -zcvf web.tar ./un1c0de.xyz
 
 expect -c "
-  spawn scp -r ./un1c0de.xyz $REMOTE_USER@$REMOTE_IP:/home
+  spawn scp -r ./web.tar $REMOTE_USER@$REMOTE_IP:/home
   expect {
     \"yes/no\" { send \"yes\n\"; exp_continue; }
     \"*assword\" { send \"$REMOTE_PASSWORD\n\"; }
